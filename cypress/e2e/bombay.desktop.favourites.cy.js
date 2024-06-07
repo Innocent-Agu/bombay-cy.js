e0974806c5093cf0c6998cdf37d8cca188ae5ccb
Innocent Agu <innocent.agu@MBP-Innocent-Agu.local>
2024-06-07 17:14:33 +0300

    my commit

--- cypress/e2e/bombay.desktop.favourites.cy.js
+++ cypress/e2e/bombay.desktop.favourites.cy.js
@@ -16,26 +16,26 @@ describe('Favourites functionality check', function() {
             cy.get('.category-highrollers').should('be.visible')
 
             // Add games to Favourites
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div[2]/div/div[1]/span').click()
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/span').click()
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div[2]/div/div/span').click()
-
+            cy.contains('Royal Riches Roulette').get('div.lobby-item-component.roulette.multiplier-roulette-variation.rrr-001.front-page.custom-bg span.star-icon').click({multiple: true});
+            cy.contains('Baruto Baccarat').get('#gameWrapperElements > div > div.generic-scrollable-container.lobby-component-scrollable-wrapper.default > div.generic-scrollable-container-inner.scrollable-container.scrollable > div > div > div > div.front-page-lists > div:nth-child(2) > div > div.carousel-slider > div > div.lobby-item-component.baccarat.classic-baccarat-variation.table-bb.front-page.custom-bg > span').click({multiple: true})
+            cy.contains('Deu Green Roulette').get('#gameWrapperElements > div > div.generic-scrollable-container.lobby-component-scrollable-wrapper.default > div.generic-scrollable-container-inner.scrollable-container.scrollable > div > div > div > div.front-page-lists > div:nth-child(3) > div > div.carousel-slider > div > div > span').click({multiple: true})
             // Verify that added games are visible in the Favourites category
             cy.get('.category-favourites').click()
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]').should('exist')
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[2]').should('exist')
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[3]').should('exist')
+            cy.get('[class="table-name"]').contains('Royal Riches Roulette').should('be.visible')
+            cy.get('[class="table-name"]').contains('Baruto Baccarat').should('be.visible')
+            cy.get('[class="table-name"]').contains('Deu Green Roulette').should('be.visible')
 
             // Remove games from Favourites
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div/span').each(($el, index) => {
+            cy.get('[data-testid="FavouriteButton"]').each(($el, index) => {
                 cy.wrap($el).click()
                 cy.wait(1000) // wait for the UI to update
             })
 
             // Verify that the removed games do not exist anymore
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]').should('not.exist')
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[2]').should('not.exist')
-            cy.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[3]').should('not.exist')
+            cy.get('.category-favourites').click()
+            cy.get('Royal Riches Roulette').should('not.exist')
+            cy.get('Baruto Baccarat').should('not.exist')
+            cy.get('Deu Green Roulette').should('not.exist')
             cy.get('.no-tables-found').contains('Currently no tables added to favourites').should('be.visible')
         })
     })
