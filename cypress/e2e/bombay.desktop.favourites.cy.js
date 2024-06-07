95f81cb7669b345ef50cc85fb22050f9d9f7c6c5
Innocent Agu <innocent.agu@MBP-Innocent-Agu.local>
2024-06-07 17:16:30 +0300

    bombaytest

--- cypress/e2e/bombay.desktop.favourites.cy.js
+++ cypress/e2e/bombay.desktop.favourites.cy.js
@@ -19,6 +19,7 @@ describe('Favourites functionality check', function() {
             cy.contains('Royal Riches Roulette').get('div.lobby-item-component.roulette.multiplier-roulette-variation.rrr-001.front-page.custom-bg span.star-icon').click({multiple: true});
             cy.contains('Baruto Baccarat').get('#gameWrapperElements > div > div.generic-scrollable-container.lobby-component-scrollable-wrapper.default > div.generic-scrollable-container-inner.scrollable-container.scrollable > div > div > div > div.front-page-lists > div:nth-child(2) > div > div.carousel-slider > div > div.lobby-item-component.baccarat.classic-baccarat-variation.table-bb.front-page.custom-bg > span').click({multiple: true})
             cy.contains('Deu Green Roulette').get('#gameWrapperElements > div > div.generic-scrollable-container.lobby-component-scrollable-wrapper.default > div.generic-scrollable-container-inner.scrollable-container.scrollable > div > div > div > div.front-page-lists > div:nth-child(3) > div > div.carousel-slider > div > div > span').click({multiple: true})
+            
             // Verify that added games are visible in the Favourites category
             cy.get('.category-favourites').click()
             cy.get('[class="table-name"]').contains('Royal Riches Roulette').should('be.visible')
